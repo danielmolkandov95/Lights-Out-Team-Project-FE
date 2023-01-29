26cb9024b749c5cc3426d2ad039009ec8f314567
danielmolkandov95 <danielmolkandov95@gmail.com>
2023-01-29 20:12:18 +0200

    home page and more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import HomePage from "./HomePage";
+
 function App() {
-  return <div>hey you</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <div className="fs-1 container mt-5 text-center font-weight-bold">
+        Hello fellow human
+      </div>
+      <div className="fs-1 container mt-5 text-center font-weight-bold">
+        Wanna play a game?
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
