20424382d0311e3288714f15d66e8e7683f780b1
Raphaelle1 <mizrahiraphaelle@gmail.com>
2023-01-30 15:29:09 +0200

    game

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import NavBar from "./components/NavBar";
 import HomePage from "./HomePage";
 import UserContextProvider from "./context/UserContext";
+import Game from "./components/Game"
 
 function App() {
   return (
     <div>
+      <UserContextProvider>
       <NavBar>
         <HomePage />
       </NavBar>
+      </UserContextProvider>
     </div>
   );
 }
