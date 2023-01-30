84980b819a94e4d61bb746bfde6ae99d3b5ce8ff
Raphaelle1 <mizrahiraphaelle@gmail.com>
2023-01-30 14:35:36 +0200

    game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,54 @@
-import React from 'react'
-import "./Game.css"
+import React from 'react';
+import "./Game.css";
+import { Table } from "react-bootstrap";
+
 
 function Game() {
   return (
-    <div className='table'></div>
+    <div>
+    <div className='score'>Your score: 100 points </div>
+     <Table className='table' striped bordered hover>
+      <tbody>
+        <tr>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+        </tr>
+        <tr>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+        </tr>
+        <tr>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+        </tr>
+        <tr>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+        </tr>
+        <tr>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+          <td><button className="light-on"></button></td>
+        </tr>
+      </tbody>
+    </Table>
+
+    </div>
+   
   )
 }
 
