9b4069919727d49adce1d993f14a581e2441eb4e
Raphaelle1 <mizrahiraphaelle@gmail.com>
2023-01-31 12:06:54 +0200

    score

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import HomePage from "./HomePage";
 import UserContextProvider from "./context/UserContext";
 import Game from "./components/Game"
 import Score from "./components/Score"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       <Score/>
-      {/* <UserContextProvider>
-        <NavBar />
-        <HomePage />
-      </UserContextProvider> */}
-      <Game/>
+      {/* <UserContextProvider>  
+       <NavBar /> 
+    <HomePage /> 
+   </UserContextProvider>  */}
+      // {/* <Game/> */}
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
